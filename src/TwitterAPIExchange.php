7bfccd9401ab8fa820a0a98e7e1410f4de6a9ce7
Jash Parekh <jash389@gmail.com>
2018-01-16 16:15:02 +0530

    Update TwitterAPIExchange.php

--- src/TwitterAPIExchange.php
+++ src/TwitterAPIExchange.php
@@ -4,12 +4,6 @@
  * Twitter-API-PHP : Simple PHP wrapper for the v1.1 API
  * 
  * PHP version 5.3.10
- * 
- * @category Awesomeness
- * @package  Twitter-API-PHP
- * @author   James Mallison <me@j7mbo.co.uk>
- * @license  MIT License
- * @link     http://github.com/j7mbo/twitter-api-php
  */
 class TwitterAPIExchange
 {
